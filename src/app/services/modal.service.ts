805350ae32749ce32400bf68a5b88d78946dd344
Alex Anackis <anackis@inbox.lv>
2023-07-16 01:22:21 -0700

    Update

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
+// @Injectable()
 export class ModalService {
+  private visible = false 
 
   constructor() { }
+
+  isModalVisible() {
+    return this.visible
+  }
+
+  toggleModal() {
+    this.visible = !this.visible
+  }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  // providers: [ModalService]
 })
 export class ModalComponent {
+
+  constructor(public modal: ModalService) {
+    // console.log(this.modal.visible)
+  }
+
+  closeModal($event: Event) {
+    this.modal.toggleModal()
+  }
 
 }
