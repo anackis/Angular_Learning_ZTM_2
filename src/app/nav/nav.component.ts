fb0822adc04a16f5b4be059ad01c06417276dba3
Alex <rengarsanj@gmail.com>
2023-07-26 08:53:19 +0300

    Update

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,8 +8,16 @@ import { ModalService } from '../services/modal.service';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  // isAuthenticated = false
 
-  constructor(public modal: ModalService) {}
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService
+  ) {
+    // this.auth.isAuthenticated$.subscribe(status => {
+    //   this.isAuthenticated = status
+    // })
+  }
 
   openModal($event: Event) {
     $event.preventDefault()
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'; 
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
+import { Observable, map } from 'rxjs';
+
 
 
 @Injectable({
@@ -11,12 +13,17 @@ import IUser from '../models/user.model';
 
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser> 
+  public isAuthenticated$: Observable<boolean>
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore,
   ) {
     this.usersCollection = db.collection('users')
+    this.isAuthenticated$ = auth.user.pipe(
+      map((user) => !!user),
+    )
+
   }
 
   public async createUser(userData: IUser) {
@@ -33,8 +40,8 @@ export class AuthService {
       throw new Error("User can not be found!")
     }
 
-    // await this.db.collection<IUser>('users').add({   // We ceated ref not to reply code in future. 
-    await this.usersCollection.doc(userCred.user.uid).set({               // Replacing add with doc and set.
+    // await this.db.collection<IUser>('users').add({               // We ceated ref not to reply code in future. 
+    await this.usersCollection.doc(userCred.user.uid).set({        // Replacing add with doc and set.
       name: userData.name,
       email: userData.email,
       age: userData.age,
