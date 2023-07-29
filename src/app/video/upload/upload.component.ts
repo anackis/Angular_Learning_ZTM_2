e0a61a5f0c886e85a6a0ffe2e1476768329d20f5
Alex <rengarsanj@gmail.com>
2023-07-29 10:31:37 +0300

    Update 29/07/2023

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,9 +1,29 @@
+
+
+
+
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html'
 })
 export class UploadComponent {
+  isDragover = false
+  file: File | null = null
+  nextStep = false
+
+  storeFile($event: Event) {
+    this.isDragover = false
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if(!this.file || this.file.type !== 'video/mp4') {
+      return 
+    }
+
+    this.nextStep = true
+
+  }
 
 }
